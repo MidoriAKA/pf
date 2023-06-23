9d26c87cc1e37a9b8f79e01d7b5d414ce5f5ea86
Midori <lorex0226@gmail.com>
2023-06-23 13:42:17 -0300

    update, add

--- js/items.js
+++ js/items.js
@@ -1,11 +1,36 @@
-function onClickITEM() {
-    function getClassByID(elementID) {
-        let element = document.getElementById(elementeID);
-        if (element) {
-            var currentClass = element.className;
-            return currentClass;
-        } else {
-            return null;
-        }
+function onClickITEM(itemNum) {
+    // function getClassByID(elementID) {
+    //     let element = document.getElementById(elementeID);
+    //     if (element) {
+    //         var currentClass = element.className;
+    //         return currentClass;
+    //     } else {
+    //         return null;
+    //     }
+    // }
+
+    let canvas = document.getElementById('canvas');
+    let item = document.getElementById('IDitem');
+    let iframe = document.getElementById('IDiframe');
+    let n1 = document.getElementById('n1');
+    let n2 = document.getElementById('n2');
+
+    // let CurrentClass = getClassByID();
+
+    canvas.classList.toggle('canvas');
+    canvas.classList.toggle('canvas-after');
+
+    item.classList.toggle('item');
+    item.classList.toggle('item-after');
+
+    if (itemNum == '1') {
+        iframe.setAttribute('src', 'items/n1.html');
+    } else if (itemNum == '2') {
+        iframe.setAttribute('src', 'items/n2.html');
     }
+
+    console.log(itemNum)
 }
+
+// TODO: CSSに変更後のクラスを記述、Display:none
+// TODO: 戻るボタン追加
