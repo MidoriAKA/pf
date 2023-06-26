b74b39be4a6f224facc5f70b7a5e6eae0fdad4cb
Midori <lorex0226@gmail.com>
2023-06-26 10:43:30 -0300

    remove comment

--- js/items.js
+++ js/items.js
@@ -1,27 +1,15 @@
 function onClickITEM(ArticleNum) {
-    // function getClassByID(elementID) {
-    //     let element = document.getElementById(elementeID);
-    //     if (element) {
-    //         var currentClass = element.className;
-    //         return currentClass;
-    //     } else {
-    //         return null;
-    //     }
-    // }
-
     let canvas = document.getElementById('canvas');
     let article = document.getElementById('IDarticle');
     let iframe = document.getElementById('IDiframe');
     let n1 = document.getElementById('n1');
     let n2 = document.getElementById('n2');
 
-    // let CurrentClass = getClassByID();
-
     canvas.classList.toggle('canvas');
-    canvas.classList.toggle('canvas-after'); // ok
+    canvas.classList.toggle('canvas-after');
 
     article.classList.toggle('article');
-    article.classList.toggle('article-after'); // ok
+    article.classList.toggle('article-after');
 
     if (ArticleNum == '1') {
         iframe.setAttribute('src', 'articles/n1.html');
@@ -41,6 +29,3 @@ function onClickRETURN() {
     article.classList.toggle('article');
     article.classList.toggle('article-after');
 }
-
-// TODO: CSSに変更後のクラスを記述、Display:none
-// TODO: 戻るボタン追加
