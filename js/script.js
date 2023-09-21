812994c274bf0ac59e3a2dc48c26d12e1e8da2d7
Midori <lorex0226@gmail.com>
2023-09-21 13:56:59 -0300

    change

--- js/script.js
+++ js/script.js
@@ -56,3 +56,34 @@ async function MenuSelected(Selected) {
             break;
     }
 }
+function LanSelected(Selected) {
+    function ShowSelected(nowSelected){
+        let SelectedVarElem = document.getElementById(`ID${nowSelected}Main`);
+        SelectedVarElem.classList.remove('HIDE');
+    }
+    function HideOther(check){
+        let HideVar = document.getElementById(`ID${check}Main`);
+        if (HideVar.classList.contains('HIDE')) {
+            ;
+        } else {
+            HideVar.classList.add('HIDE');
+        }
+    }
+    switch (Selected) {
+        case 'en':
+            ShowSelected('en');
+            HideOther('ja');
+            HideOther('pt');
+            break;
+        case 'ja':
+            ShowSelected('ja');
+            HideOther('en');
+            HideOther('pt');
+            break;
+        case 'pt':
+            ShowSelected('pt');
+            HideOther('en');
+            HideOther('ja');
+            break;
+    }
+}
